5b04595bb1955767f7edc3ee00e7d04229feb734
TheBilo16 <billy_paredes-2000@hotmail.com>
2020-09-21 19:46:10 -0500

    Arreglo del host

--- build/config/index.js
+++ build/config/index.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.tokenKey = exports.database = void 0;
 exports.database = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bb0066cd3dac83',
+    password: '6157c3ea',
     dbname: 'multi_services'
 };
 exports.tokenKey = 'team-proyect-X';
