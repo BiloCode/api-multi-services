3ebc489fc1e237086dc6c97fc0cbded7999df14e
TheBilo16 <billy_paredes-2000@hotmail.com>
2020-09-21 22:53:16 -0500

    Arreglo de

--- src/database/models/Province.ts
+++ src/database/models/Province.ts
@@ -2,6 +2,11 @@ import { DataTypes } from "sequelize";
 import sequelize from "..";
 
 const Province = sequelize.define('province',{
+  id : {
+    type : DataTypes.INTEGER,
+    autoIncrement : true,
+    primaryKey : true
+  },
   name : DataTypes.STRING,
 },{
   timestamps : false
