4ce0a09c7185890f49a3e201359cc548d0641fd9
TheBilo16 <billy_paredes-2000@hotmail.com>
2020-09-21 19:41:18 -0500

    Arreglo del host

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const database = {
-  host : 'mysql://bb0066cd3dac83:6157c3ea@us-cdbr-east-02.cleardb.com/heroku_7ba0cb12424997c?reconnect=true',
+  host : 'us-cdbr-east-02.cleardb.com',
   user : 'bb0066cd3dac83',
   password : '6157c3ea',
   dbname : 'multi_services'
