dfef781944f6fd36c927ad37dcd55cee5612b4ec
BiloCode <billy_paredes_aycho@hotmail.com>
2020-10-31 17:33:07 -0500

    Arreglos de conexion con mongoDb

--- src/application/database/mongodb/index.ts
+++ src/application/database/mongodb/index.ts
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 
 (async function(){
   try {
-    await connect(`mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}`, {
+    await connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@host${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}`, {
       useNewUrlParser : true,
       useUnifiedTopology: true
     });
